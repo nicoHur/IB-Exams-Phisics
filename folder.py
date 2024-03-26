5786228a78658262bd9835fba1525a46e32c8268
Rafa Ponce Vivancos <93051606+raf181@users.noreply.github.com>
2024-03-26 15:48:14 +0100

    Update

---
+++ folder.py
@@ -0,0 +1,17 @@
+import os
+
+# Define the range of years
+years = range(1999, 2025)
+
+# Define the months
+months = ["May", "November"]
+
+# Iterate over each year
+for year in years:
+    # Create a directory for the year if it does not exist
+    os.makedirs(str(year), exist_ok=True)
+    
+    # Iterate over each month
+    for month in months:
+        # Create a directory for the month inside the year directory if it does not exist
+        os.makedirs(os.path.join(str(year), month), exist_ok=True)
